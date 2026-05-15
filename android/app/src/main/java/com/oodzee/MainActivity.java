8fd50d104ad3ad34eea8f40004e82fce91a4dd51
Yash Gupta <yg123yash@gmail.com>
2026-05-15 12:50:14 +0530

    working

--- android/app/src/main/java/com/oodzee/MainActivity.java
+++ android/app/src/main/java/com/oodzee/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this, R.style.SplashStatusBarTheme);
+        SplashScreen.show(this, R.style.SplashStatusBarTheme, false);
         super.onCreate(savedInstanceState);
     }
 
